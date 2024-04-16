963686aa846244d044c0f1c4986adc6bafe230c0
Antonio <Caparrini@users.noreply.github.com>
2024-04-16 09:47:43 +0000

    Done Task http-client.component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,12 @@ import { provideMarkdown } from 'ngx-markdown'
 import { HttpClient, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http'
 import { baseUrlInterceptor } from './exercises/components/http-client/_solution/base-url.interceptor'
 import { errorInterceptor } from './exercises/components/http-client/_solution/error.interceptor'
+import { cinterceptor } from './exercises/components/http-client/cinterceptor'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withFetch(), withInterceptors([baseUrlInterceptor, errorInterceptor])),
+    provideHttpClient(withFetch(), withInterceptors([baseUrlInterceptor, errorInterceptor, cinterceptor])),
     provideMarkdown({ loader: HttpClient }),
   ],
 }
